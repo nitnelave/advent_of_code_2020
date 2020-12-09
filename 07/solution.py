10bbd754f48b819d540dee9c79ae80e25d501838
Valentin Tolmer <valentin.tolmer@gmail.com>
2020-12-09 12:23:27 +0100

    day 07: slight improvements, comments

--- 07/solution.py
+++ 07/solution.py
@@ -7,29 +7,32 @@ def parse_rule(line):
         return (color, {})
     return (color, {c: int(n) for (n, c) in [re.fullmatch(r'(\d+) (.+) bags?', bag).groups() for bag in contents.split(', ')]})
 
+# {color -> {contained_color -> amount}}
 rules = dict(map(parse_rule, open('input')))
+# {contained_color -> [containing_color]}
 reversed_rules = defaultdict(set)
 for color, contents in rules.items():
     for c in contents.keys():
         reversed_rules[c].add(color)
 
+# Random-order tree traversal.
 can_contain_shiny_gold = reversed_rules['shiny gold'].copy()
 to_explore = can_contain_shiny_gold.copy()
-explored = set()
 while len(to_explore) != 0:
     c = to_explore.pop()
-    explored.add(c)
     for container in reversed_rules[c]:
-        can_contain_shiny_gold.add(container)
-        if not container in explored:
+        if container not in can_contain_shiny_gold:
             to_explore.add(container)
+            can_contain_shiny_gold.add(container)
 
 print(len(can_contain_shiny_gold))
 
+# DFS tree traversal.
 def count_bag_content(color):
     count = 1
     for c, n in rules[color].items():
         count += n * count_bag_content(c)
     return count
 
+# The shiny gold bag doesn't contain itself.
 print(count_bag_content('shiny gold') - 1)
